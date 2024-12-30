97bfbfda14968245e803459f4c96946061eebfd5
sandevD <sandev.net@gmail.com>
2024-12-31 04:52:48 +0530

    🎨 ✨ Refactor CrustCustomizationHandler to use ColorCodes for success message output

--- Chain/CrustCustomizationHandler.java
+++ Chain/CrustCustomizationHandler.java
@@ -2,6 +2,7 @@ package Chain;
 
 import Core.Pizza.CustomPizza;
 import Core.Pizza.Pizza;
+import Utils.ColorCodes;
 
 public class CrustCustomizationHandler extends CustomizationHandler {
     @Override
@@ -9,7 +10,7 @@ public class CrustCustomizationHandler extends CustomizationHandler {
         if (customization.toLowerCase().contains("crust")) {
             String crustType = customization.split(":")[1].trim();
             ((CustomPizza) pizza).setCrust(crustType);
-            System.out.println("Crust customized to: " + crustType);
+            ColorCodes.printSuccessMessage("\nCrust customized to: " + crustType, true);
         }
         return passToNextHandler(pizza, customization);
     }
