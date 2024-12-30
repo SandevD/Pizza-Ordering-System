b209f251a09e6361ce43cf36ff822503465a6056
sandevD <sandev.net@gmail.com>
2024-12-31 04:52:53 +0530

    🎨 Refactor ExtraCheeseCustomizationHandler to use ColorCodes for success message output

--- Chain/ExtraCheeseCustomizationHandler.java
+++ Chain/ExtraCheeseCustomizationHandler.java
@@ -2,13 +2,14 @@ package Chain;
 
 import Core.Pizza.Pizza;
 import Decorator.ExtraCheeseDecorator;
+import Utils.ColorCodes;
 
 public class ExtraCheeseCustomizationHandler extends CustomizationHandler {
     @Override
     public Pizza handleRequest(Pizza pizza, String customization) {
         if (customization.toLowerCase().contains("extra cheese")) {
             Pizza decoratedPizza = new ExtraCheeseDecorator(pizza);
-            System.out.println("Added extra cheese");
+            ColorCodes.printSuccessMessage("\nAdded extra cheese!", true);
             return decoratedPizza;
         } else {
             return passToNextHandler(pizza, customization);
